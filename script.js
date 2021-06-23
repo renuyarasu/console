91613ec5d82692f2d4d16c273a70f6d287cceeae
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-23 23:21:02 +0530

    console.time | timeEnd | timeLog

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Javascript console Methods
 console.clear(); // Console was cleared
 
-// console.count();
-// console.countReset();
+// console.time();
+// console.timeLog();
+// console.timeEnd();
 
-for( i = 0; i < 10; i++){
-    console.count();
-}
+console.time();
+
+setTimeout(() => {
+    console.timeEnd();
+}, 5000) //default: 5015.69091796875 ms
+
+setTimeout(() => {
+    console.timeLog();
+}, 2000) // default: 2014.47314453125 ms
