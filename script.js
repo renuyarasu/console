67fd197f7187ba042df8be6509acb1d9743ae7f8
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-23 23:05:31 +0530

    console.assert

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Javascript console Methods
 console.clear(); // Console was cleared
 
-const user = {
-    name: 'VedaGna',
-    age: 5,
-}
-console.log(user); //The console.log() method prints out a toString representation of the object in the console to the user.
+// console.assert(); 
+// The console.assert() method writes an error message to the console if the assertion is false. If the assertion is true, nothing happens.
 
-console.dir(user); // The console.dir() method output the list of object properties of a specified object in the console to the user.
+
+const isItWorking = true;
+console.assert(isItWorking, 'This is the reason console.assert!');
+
 
