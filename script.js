5be7dd9451b5ebd84f12ffeea7e9ce919755af18
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-23 22:51:08 +0530

    console.log - variables

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 // Javascript console Methods
-
 console.clear();
-const employee = {
-    name: 'VedaGna',
-    designation: 'DEV'
-}
-const
-    x = 1,
-    y = 2,
-    z = 3;
 
-// console.log(x, y, z); // 1 2 3
-// console.log({x, y, z}); // { x: 1, y: 2, z: 3 }
-// console.log(employee); // VedaGna
-console.log({ employee }); // { employee: { name: 'VedaGna', designation: 'DEV' } }
+// variables
+// %s string
+// %d digits
+
+console.log('%s is %d years old.', 'VedaGna', 5); // VedaGna is 5 years old.
+
+
+
