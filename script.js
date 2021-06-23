bf7d3f30b9fa8d38a8c25ac7f40a21d87ec99e90
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-23 23:58:02 +0530

    console.degug();

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 // Javascript console Methods
 console.clear(); // Console was cleared
 
-// console CSS styles
-
-console.log(
-  "%c Auth ",
-  "color:white; background-color: blue",
-  "User has logged in."
-);
-console.log(
-  "%c Info ",
-  "color:black; background-color: yellow; font-weight:bold;",
-  "User has logged in."
-);
-console.log(
-  "%c Error ",
-  "color:white; background-color: red; font-size: 1rem",
-  "User has logged in."
-);
+// console.debug() - Debugger
+for (i = 0; i < 3; i++){
+    debugger
+    console.log(i);
+}
