1f9f51e22f86cbb222f86ffa9247682580bc8fbc
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-23 22:40:19 +0530

    console.log - Object

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const employee = {
     name: 'VedaGna',
     designation: 'DEV'
 }
+const
+    x = 1,
+    y = 2,
+    z = 3;
 
-jsCode = async () => {
-    console.log('VedaGna DEV');
-}
-jsCode();
+// console.log(x, y, z); // 1 2 3
+// console.log({x, y, z}); // { x: 1, y: 2, z: 3 }
+// console.log(employee); // VedaGna
+console.log({ employee }); // { employee: { name: 'VedaGna', designation: 'DEV' } }
