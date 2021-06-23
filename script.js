f739698ba5e44580a55e5c93db41b58d9c557d14
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-23 23:31:24 +0530

    console.trace()

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Javascript console Methods
 console.clear(); // Console was cleared
 
-// console.group();
-// console.groupCollapsed();
-// console.groupEnd();
-
-// console.group('Group');
-console.groupCollapsed('Group');
-console.log('Lorem ipsum dolor sit amet.');
-console.log('Lorem ipsum dolor sit amet.');
-console.log('Lorem ipsum dolor sit amet.');
-console.log('Lorem ipsum dolor sit amet.');
-console.groupEnd();
-console.log("I'm not in teh group!");
+// console.trace();
+function one() {
+    two()
+}
+function two() {
+    three()
+}
+function three() {
+    four()
+}
+function four() {
+    console.trace()
+}
